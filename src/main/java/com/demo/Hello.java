4efc20440e8d80be34262a6de5fda47a4979113d
junjia <2280133426@qq.com>
2020-11-28 22:35:20 +0800

    add double

--- src/main/java/com/demo/Hello.java
+++ src/main/java/com/demo/Hello.java
@@ -3,6 +3,6 @@ package com.demo;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("hahaha");
+        System.out.println("double");
     }
 }
